b9f21ef7afe3e9928ebfd1e32bca5c006a4da444
Danilo Montecel <daniloomarmontecel@gmail.com>
2024-01-12 00:49:15 -0500

    modificando la segunda version

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 ///--Modo osuro--///
 
+const iconModo=document.querySelector(".iconSun");
+iconModo.addEventListener("click",()=>{
+    if(document.body.classList.toggle("light"))return(iconModo.textContent="🌑");
+    iconModo.textContent="☀️"
+})
 
 //------///
 btnOpen.onclick=function(){
